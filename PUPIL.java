55e3d08e2b2992f579b0e507c9781479717d1899
claudia123456 <Guest@stk-cs01-ws12.westlothian.sch.uk>
2015-11-25 12:11:08 +0000

    1.1
    i completed 1.1

--- PUPIL.java
+++ PUPIL.java
@@ -1,15 +1,39 @@
 public class PUPIL
 {
-    //properties
-    
+    // list of properties
     private String fName;
     private String sName;
-    private int PupilMark;
-    
-
+    private Float PupilMark;
     public PUPIL ()
     {
-       //constructor
+        //constructor
+
+        fName = "";
+        sName = "";
+        PupilMark = 0.0f;
+
+    }      
+    //file handling store details from fie
+    public void readPupilDetails (String dataItems)
+    {//unpack string of row data into fields
+        String [] rowItems = dataItems.split(",");
+        // store each data items as instance property
+        fName = rowItems [0];
+        sName = rowItems [1];
+        PupilMark = Float.parseFloat(rowItems[2]);
+    }  
+
+    public String writeDetails ()
+    {
+        // Join up data into a string to output as a row
+        //use "," to seperate csv colums
+        String pupilData = "";
+        pupilData= pupilData.concat(fName);
+        pupilData = pupilData.concat(",");
+        pupilData = pupilData.concat(sName);
+        pupilData = pupilData.concat(",");
+        pupilData = pupilData.concat(Float.toString(PupilMark));
+        return pupilData;
     }
     
-}
+}    

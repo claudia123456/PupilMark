353b67dbe68c9b5c4d7936f93cf3ae1cdd1ea92a
claudia123456 <Guest@stk-cs01-ws12.westlothian.sch.uk>
2015-11-26 09:38:09 +0000

    sprint 2.1

---
+++ SCHOOL.java
@@ -0,0 +1,28 @@
+public class SCHOOL
+{
+   // array of PUPIL objects
+   private PUPIL pupilList[];
+   // number of pupils to be called 
+   int noOfPupils;
+   public SCHOOL()
+   {
+
+   }
+
+   // top level algorithm
+   public void processPupils()
+   {
+      setupPupilList();
+      countPupilMark();
+     
+   }
+   
+    private void setupPupilList()
+    { // placeholder 
+    }
+    public void countPupilMark()
+    {
+        //placeholder
+    }
+
+}
